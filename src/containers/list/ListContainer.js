3b8a65a4a38edfa4ff5ab5250f96de7dcae85cd5
blangcos <blangcos@naver.com>
2019-08-19 21:55:54 +0900

    템플릿 작성

--- src/containers/list/ListContainer.js
+++ src/containers/list/ListContainer.js
@@ -6,14 +6,10 @@ import {
 } from 'semantic-ui-react'
 import style from 'style/style'
 import { SampleList } from 'components'
-import axios from  'axios';
-
-/** 리덕스 연결을 위해 필요한 모듈  **/
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
-
-/** modules에서 선언한 액션함수 및 reducer를 가져온다 **/
-import * as listActions from 'modules/list';
+import axios from  'axios'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import * as listActions from 'modules/list'
 
 class ListContiner extends React.Component {
     /** 생성자 */
@@ -30,24 +26,23 @@ class ListContiner extends React.Component {
   
     /** 저장된 리스트 조회 */
     handleGetList = () => {
+      const { ListAction } = this.props;
 
-        const { ListAction } = this.props;
-
-        axios({
-          url:"/sample",
-          method:"get",
-          headers: { "Pragma": 'no-cache'}  
-        })
-        .then( (response) => {
-          if (response == null){
-              console.log('response is null!');
-          }else {
-              //조회한 데이터 store에 셋팅
-              ListAction.setList(response.data);
-          }
-        }).catch(function(error) {
-          console.log(error.response);
-        });
+      axios({
+        url:"/sample",
+        method:"get",
+        headers: { "Pragma": 'no-cache'}  
+      })
+      .then( (response) => {
+        if (response == null){
+            console.log('response is null!');
+        }else {
+            //조회한 데이터 store에 셋팅
+            ListAction.setList(response.data);
+        }
+      }).catch(function(error) {
+        console.log(error.response);
+      });
     }
 
     render() {
@@ -66,21 +61,11 @@ class ListContiner extends React.Component {
     }
 }
 
-// 컴포넌트에 리덕스 연결시 아래 형태 connect - state - dispatch 사용
 export default connect(
-    //component에서 사용하기 위해 store에서 저장된 값을 꺼내서 할당하는 부분
-    //this.props.list
-    //this.props.userId 이런식으로 접근
     (state) => ({
-        /* ↓ 현재 component에서 사용될 props 명 */
-        list: state.list.get('list'), /* faqList라는 props 내부 변수에 store에 저장되어 있는 list를 get해서 저장 */
-                  /* ↑ store의 state에서 꺼내는 부분 */             
+        list: state.list.get('list'),
     })
-    //현재 component에서 사용하기 위해 reducer에서 가져온 함수를 할당하는 부분
-    //this.props.FaqAction
     , (dispatch) => ({
-        /* ↓ 현재 component에서 사용될 props 명 */
-        ListAction : bindActionCreators(listActions, dispatch), /*상단에서 선언한 faqActions(modules)에 만들어놓은를 가져와서 FaqAction에 넣는다 */
-                                        /* ↑ modules 폴더에서 선언한 내용 */
+        ListAction : bindActionCreators(listActions, dispatch)
     })
 )(ListContiner);
